2d0e95a60419bdfcf91cd37917f84b7c946067cc
Asabaal Horan <asabaalhoran@gmail.com>
2026-01-06 15:11:59 -0600

    brick expansion

--- implementation/take_selector_v2/backend/take_selector/__init__.py
+++ implementation/take_selector_v2/backend/take_selector/__init__.py
@@ -6,6 +6,7 @@ from .data_loader import load_transcript, Segment, Transcript, load_transcript_f
 from .similarity import compute_segment_similarity, find_candidate_duplicates
 from .grouping import Group, GroupManager
 from .selection import SelectionManager
+from .workflow import TakeSelectorWorkflow, WorkflowState
 
 __all__ = [
     'load_transcript',
@@ -16,5 +17,7 @@ __all__ = [
     'find_candidate_duplicates',
     'Group',
     'GroupManager',
-    'SelectionManager'
+    'SelectionManager',
+    'TakeSelectorWorkflow',
+    'WorkflowState'
 ]
